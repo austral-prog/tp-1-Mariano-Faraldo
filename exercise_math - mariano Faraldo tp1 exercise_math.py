53746e4ee9695b2047e4cce90b2464fac38540fa
Mariano-Faraldo <mfaraldo@mail.austral.edu.ar>
2025-08-16 11:11:03 -0300

    Add files via upload
    Aqui esta el tp1 profe! los separe a los ejercicios por asi es mas comodo corregir punto 1 y 2!
    
    - Mariano Faraldo

---
+++ exercise_math - mariano Faraldo tp1 exercise_math.py
@@ -0,0 +1,20 @@
+# 2- Escribir un programa dentro de exercise_math.py que dado dos números enteros imprima en pantalla el resultado de las siguientes operaciones: 
+# la suma, la diferencia, el producto, el promedio, el cociente entero y el resto de la división entera y el valor real de la división. 
+# Para entregar correctamente se deberá imprimir dichos resultados en el orden que fueron pedidos en la consigna. 
+# Por ejemplo, primero la suma, despues la diferencia, y asi sucesivamente.
+
+# Ejemplo: Para a = 57 y b = 7 el output debera ser:
+
+def math():
+    a = 57
+    b = 7
+    print (a + b)
+    print (a - b)
+    print (a * b)
+    print (a + b / 2)
+    print (int(a / b))
+    print (int(a % b))
+    print (a // b)
+math()
+
+# Mariano Faraldo
---
+++ math_test - mariano faraldo tp 1 math_test.py
@@ -0,0 +1,44 @@
+#1- Para este ejercicio el objetivo es leer el código, anotar lo que piensen que los prints vayan a decir y luego correr el código para ver si les dio igual o no.
+
+i1 = 3 # aca asignamos un valor en una variable
+i2 = 5 # aca asignamos un valor en una variable x2
+i3 = i2 + i1 # aca se asiga la suma entre i1 e i2
+print("valor de i1:") 
+print(i1) # aca se imprime el valor i1 con el anterior print de "valor de i1:"
+print("valor de i2:")
+print(i2) # aca se imprime el valor i2 con el anterior print de "valor de i2:"
+print("valor de i3:")
+print(i3) # aca se imprime el valor i3 
+print(i1 + i2 + i3) # aqui se sumaron todos los valores y se lo mostraron en pantalla
+
+s1, s2, s3 = "Python", " is ", 'awesome' # se le asigna a cada uno de las variables un valor, a s1 "python", a s2 "is", a s3 "awesome"
+print(s1 + s2 + s3) # aqui se juntan los 3 strings para formar una oracion.
+
+x = y = z = "Naranja" # aca se le asiga a x, y, z el valor de "Naranja"
+print("valor de x: " + x + ", valor de y: " + y + ", valor de z: " + z) # aca se concatenan el valor de x, y, z 
+
+z1 = i3 / i2
+print(z1) # aca se imprime el dividendo de la operacion i3 / i2
+z2 = i3 % i2
+print(z2) # aca se imprime el residuo del modulo de i3 % i2
+
+f1 = -.5 # aqui se le asigna el valor de -.5 a f1
+f2 = 10  # aqui se le asiga el valor de 10 a f2
+f3 = f1 + f2 # la suma entre estos se le asignara a la variable de f3
+i3 = int(f3) # aqui asignas a i3 el valor convertido entero de f3
+
+print("entero i3:")
+print(i3) # aqui imprime i3, mostrando que sera un entero
+print("variable f3:")
+print(f3) # aca imprime la variable flotante de f3
+
+f2 += i1 # aca se suma el f2 a i1
+print("el valor de")
+print(f2) # aca muestran el valor de f2
+print("más")
+print(f1) # aca muestran el valor de f1
+print("es:")
+print(f2 + f1) # aca muestran la suma de ambos
+
+# al final, el codigo andubo como yo esperaba
+# Mariano Faraldo
