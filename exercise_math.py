4e386de73776c618d15b6a4f90f45a81d1d60dd5
Mariano-Faraldo <mfaraldo@mail.austral.edu.ar>
2025-08-18 10:59:03 -0300

    Update exercise_math.py
    Mariano Faraldo
    tp 2

--- exercise_math.py
+++ exercise_math.py
@@ -1,3 +1,13 @@
 def math():
     a = 57
     b = 7
+    print (a + b)
+    print (a - b)
+    print (a * b)
+    print (a + b / 2)
+    print (int(a / b))
+    print (int(a % b))
+    print (a // b)
+math()
+
+# Mariano Faraldo
