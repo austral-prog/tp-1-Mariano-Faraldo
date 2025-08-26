035876fe07913dfe71f99c44912a15bffae4e05e
Mariano-Faraldo <mfaraldo@mail.austral.edu.ar>
2025-08-26 20:45:02 -0300

    Add files via upload
    aqui esta el tp1 resubido y corregido :D

--- exercise_math.py
+++ exercise_math.py
@@ -1,13 +1,28 @@
 def math():
     a = 57
     b = 7
-    print (a + b)
-    print (a - b)
-    print (a * b)
-    print (a + b / 2)
-    print (int(a / b))
-    print (int(a % b))
-    print (a // b)
+
+    suma = a + b 
+    print(suma)
+
+    diferencia = a - b
+    print(diferencia)
+
+    producto = a * b
+    print(producto)
+
+    promedio = (a + b) / 2
+    print(promedio)
+
+    cociente_entero = a // b
+    print(cociente_entero)
+
+    resto = a % b
+    print(resto) 
+
+    divicion_real = a / b
+    print(divicion_real)
+
 math()
 
 # Mariano Faraldo
